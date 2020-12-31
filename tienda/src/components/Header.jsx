f5afe99602396e0c8ec45ae971892ccbd7ef5033
LAPTOP-F07M110C\Moni <monicayzm@hotmail.com>
2020-12-31 00:44:17 -0300

    navbar

---
+++ tienda/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './../styles.css'; 
+
+const Header = () => {
+    return (
+       
+            <header>
+                <nav id="navBar">
+                    <p>logo</p>
+                    <ul>
+                        <li>Inicio</li>
+                        <li>Tienda</li>
+                        <li>Contacto</li>
+                        <li>Carrito</li>
+                    </ul>
+                </nav>
+            </header>
+        
+    )
+}
+
+export default Header
