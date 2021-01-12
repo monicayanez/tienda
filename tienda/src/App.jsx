8fddf4cf5d0684c77b3af49fd28e10e062918e0b
LAPTOP-F07M110C\Moni <monicayzm@hotmail.com>
2021-01-11 22:49:39 -0300

    entrega promises

--- tienda/src/App.jsx
+++ tienda/src/App.jsx
@@ -1,19 +1,72 @@
 
-import React from 'react';
+
+import React , { useState, useEffect } from 'react';
 import './styles.css'; 
 import 'materialize-css';
 import 'materialize-css/dist/css/materialize.css';
 import Header from './components/Header';
-import ItemListContainer from './components/ItemListContainer';
+import ItemListContainer from './components/ItemListContainer.jsx';
 
-function App(){ 
-    return (
-<>
-<Header />
-<ItemListContainer />
-</>
-)
+
+const productsAPI = [ 
+    { 
+        id: "0",
+        productName: "sticker0",
+        description: "Tamaños y medidas personalizables",
+        price: 200,
+        productImg: "https://i.pinimg.com/originals/e0/4d/04/e04d0447d760fb60aa12c8df7c44321e.jpg"
+    },
+    { 
+        id: "1",
+        productName: "sticker1",
+        description: "Tamaños y medidas personalizables",
+        price: 200,
+        productImg: "https://i.pinimg.com/originals/e0/4d/04/e04d0447d760fb60aa12c8df7c44321e.jpg"
+    },
+    { 
+        id: "2",
+        productName: "sticker2",
+        description: "Tamaños y medidas personalizables",
+        price: 200,
+        productImg: "https://i.pinimg.com/originals/e0/4d/04/e04d0447d760fb60aa12c8df7c44321e.jpg"
+    },
+    { 
+        id: "3",
+        productName: "sticker3",
+        description: "Tamaños y medidas personalizables",
+        price: 200,
+        productImg: "https://i.pinimg.com/originals/e0/4d/04/e04d0447d760fb60aa12c8df7c44321e.jpg"
+    },
+    { 
+        id: "4",
+        productName: "sticker4",
+        description: "Tamaños y medidas personalizables",
+        price: 200,
+        productImg: "https://i.pinimg.com/originals/e0/4d/04/e04d0447d760fb60aa12c8df7c44321e.jpg"
+    },
+]
+
+export default function App() {
+      
+    const [ items, setItems ] = useState([])
+
+    useEffect(() => {
+      const promesa = new Promise((resolve, reject)=>{
+        setTimeout(function(){
+          resolve(productsAPI); 
+        }, 2000);
+      }
+      )
+      promesa.then( result => setItems(result)) 
+      promesa.catch( err => console.log("Algo salio mal")) 
+  
+    }, []);
+
+return (
+    <>
+   <Header/>
+   {items.map(product => <ItemListContainer id={product.id} nombre={product.productName} precio={product.price} imagen={product.productImg} descripcion={product.description} /> )} 
+
+    </>
+  );
 }
-
-export default App
-
--- tienda/src/components/ItemListContainer.jsx
+++ tienda/src/components/ItemListContainer.jsx
@@ -1,111 +1,29 @@
-import React , {useState,useEffect} from 'react'
+import React from 'react';
+import 'materialize-css';
+import 'materialize-css/dist/css/materialize.css';
 
-const items = [
-    { 
-        id: "0",
-        productName: "sticker0",
-        description: "Tamaños y medidas personalizables",
-        price: 200,
-        productImg: "https://i.pinimg.com/originals/e0/4d/04/e04d0447d760fb60aa12c8df7c44321e.jpg"
-    },
-    { 
-        id: "1",
-        productName: "sticker1",
-        description: "Tamaños y medidas personalizables",
-        price: 200,
-        productImg: "https://i.pinimg.com/originals/e0/4d/04/e04d0447d760fb60aa12c8df7c44321e.jpg"
-    },
-    { 
-        id: "2",
-        productName: "sticker2",
-        description: "Tamaños y medidas personalizables",
-        price: 200,
-        productImg: "https://i.pinimg.com/originals/e0/4d/04/e04d0447d760fb60aa12c8df7c44321e.jpg"
-    },
-    { 
-        id: "3",
-        productName: "sticker3",
-        description: "Tamaños y medidas personalizables",
-        price: 200,
-        productImg: "https://i.pinimg.com/originals/e0/4d/04/e04d0447d760fb60aa12c8df7c44321e.jpg"
-    },
-    { 
-        id: "4",
-        productName: "sticker4",
-        description: "Tamaños y medidas personalizables",
-        price: 200,
-        productImg: "https://i.pinimg.com/originals/e0/4d/04/e04d0447d760fb60aa12c8df7c44321e.jpg"
-    },
-]
+function ItemListContainer ({id, productName, description, price, productImg}) {
 
-const ItemListContainer = () => {
-    const [qty, qtyItems] = useState(0)
-
-    const addItem = () => { 
-        qtyItems(qty + 1)
-    }
-    const deleteItem = () => {
-        qtyItems(qty - 1)
-    }
-    const restart = () => { 
-        qtyItems(0)
-    }
-
-    const[item,setItem] = useState()
-    useEffect(()=>{
-      let promise = new Promise ((res,rej)=>{
-        setTimeout(()=>{
-            res(items)
-        },2000)
-      })
-    })
-
-    useEffect(()=>{
-        console.log("solo al principio")
-        fetch (items)
-        .then((res) =>{
-            return res.json()
-        })
-        .then((res)=>{
-            console.log(res)
-        })
-    },[])
-
-    useEffect(()=>{
-        console.log("solo si qty cambia")
-    },[qty])
-
-    const toggle = () => {
-        setItem(!item)
-    }
-    
-
-    return (
-        <container> 
-                <div class="row">
-                    <div class="col s12 m6">
-                        <div class="card">
-                            <div class="card-image">
-                                <img src={productImg} />
-                                <span class="card-title">{productName}</span>
-                                <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-                            </div>
-                            <div class="card-content">
-                                <p>{description}</p>
-                                <p>{price}</p>
-                            </div>
-                            <p>Cantidad de artículos {qty}</p>
-                            <button onClick={addItem}>+</button>
-                            <button onClick={deleteItem}>-</button>
-                            <button onClick={restart}>Eliminar articulos</button>
-                            <div>
-                                <button onClick={toggle}>Agregar al carrito</button>
-                            </div>
-                        </div>
-                    </div>
+ 
+  return (
+    <>
+    <div class="row">
+        <div class="col s12 m6" id={id}>
+            <div class="card">
+                <div class="card-image">
+                <img src={productImg}/>
+                <span class="card-title">{productName}</span>
+                <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add_shopping_cart</i></a>
                 </div>
-            </container>      
-    )
+                <div class="card-content">
+                <p>{description}</p>
+                <p>{price}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </>
+  )
 }
 
-export default ItemListContainer
+export default ItemListContainer    
